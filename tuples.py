df5c4b5d2e5c0c6dccfa6e094f18c5642ee4405c
Swetha-manikandan <swetha030405@gmail.com>
2026-06-23 13:07:29 +0530

    set

---
+++ tuples.py
@@ -0,0 +1,13 @@
+T = (12.5,3,"ABC")
+T[1]
+print(T[1])
+
+a = (1,2,1,3)
+print(a.count(1))
+print(a.index(1))
+
+S = (10,11,26,25,24)
+print(S[0])
+print(S[4])
+
+fruits = ('apple', 'banana', 'Orange','mango')
